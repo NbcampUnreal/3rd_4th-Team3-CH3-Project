eb7a060d2d54c69e1003a9faeca4e3c6a0d73877
gussen33 <gussen1221@gmail.com>
2025-08-13 11:18:26 +0900

    add tank zombie animation

---
+++ Source/LightHouseDefense/Public/TankZombieCharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AZombieCharacter.h"
+#include "TankZombieCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIGHTHOUSEDEFENSE_API ATankZombieCharacter : public AAZombieCharacter
+{
+	GENERATED_BODY()
+public:
+    //생성자
+    ATankZombieCharacter();
+};
